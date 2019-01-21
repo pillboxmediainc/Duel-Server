62059143e97f7291dc0d402be8e7670e8455b728
Josh Feinstein <pillboxmediainc@gmail.com>
2019-01-21 17:30:06 -0500

    added socket.on for iwin

--- socket/index.js
+++ socket/index.js
@@ -1,13 +1,23 @@
 module.exports = io => {
   io.on('connection', socket => {
     console.log(socket.id, ' has made a persistent connection to the server!');
+    // socket.emit('join-game', 'can you hear me?');
 
-    socket.on('new-message', message => {
-      socket.broadcast.emit('new-message', message);
+    socket.on('iwin', () => {
+      socket.broadcast.emit('youlose');
+      // io.broadcast.emit('youlose');
     });
 
-    socket.on('new-channel', channel => {
-      socket.broadcast.emit('new-channel', channel);
+    // socket.on('new-channel', channel => {
+    //   socket.broadcast.emit('new-channel', channel);
+    // });
+
+    //   socket.emit('request', /* */); // emit an event to the socket
+    // io.emit('broadcast', /* */); // emit an event to all connected sockets
+    // socket.on('reply', function(){ /* */ }); // listen to the event
+
+    socket.on('disconnect', () => {
+      console.log(socket.id, 'has disconnected from the server');
     });
   });
 };
